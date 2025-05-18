afb9f531b2dc246cbaba75654edff49a5b22e431
adarshkumar790 <126936165+adarshkumar790@users.noreply.github.com>
2025-05-18 20:38:28 +0530

    origin added

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true, // if you need cookies/auth
 }));
